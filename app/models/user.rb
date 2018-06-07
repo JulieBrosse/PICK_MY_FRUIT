3de550640762f27d8c9ae00ceb27b68e33b36a69
Julie Brosse <juliebrosse31@gmail.com>
2018-06-07 20:39:51 +0200

    model user ok without position

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum status: { picker: 1, owner: 2 }
+  validates :status, presence: true
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :gender, presence: true
+#  validates :photo, allow_blank:false
+#  validates :position,
+
   has_many :orders
   has_many :plantings, foreign_key: "owner_id"
+
 end
