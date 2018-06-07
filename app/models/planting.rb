74d6a85457a00474a4a21feb0c5fe80ebff08488
Julie Brosse <juliebrosse31@gmail.com>
2018-06-07 12:46:48 +0200

    ok models

--- app/models/planting.rb
+++ app/models/planting.rb
@@ -1,2 +1,4 @@
 class Planting < ApplicationRecord
+  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
 end
+
---
+++ db/migrate/20180607103705_remove_fruits_from_harvest.rb
@@ -0,0 +1,4 @@
+class RemoveFruitsFromHarvest < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
