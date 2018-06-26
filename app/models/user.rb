01b2e1fcd24635004b4b5576fff8305ab4816997
Julie Brosse <juliebrosse31@gmail.com>
2018-06-26 22:06:31 +0200

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
-#  enum status: { picker: 1, owner: 2 }
-#  validates :status, presence: true
-#  validates :name, presence: true
-#  validates :age, presence: true
-#  validates :gender, presence: true
+  # has_many :orders
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many :plantings
+  #  enum status: { picker: 1, owner: 2 }
 
-
-
-#  validates :photo, allow_blank:false
-#  validates :position,
-
-  has_many :orders
-#  has_many :plantings, foreign_key: "owner_id"
-
+  #  validates :photo, allow_blank:false
+  #validates :position,
 end
